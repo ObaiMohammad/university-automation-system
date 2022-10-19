901fa8baa4d93abcbee6064d71c7771d7fd38195
ObaiMohammad <obai.mohamed.bis@gmail.com>
2022-10-19 14:50:22 +0200

    first commit

---
+++ src/test/java/it/unipd/University_automation_system/AppTest.java
@@ -0,0 +1,20 @@
+package it.unipd.University_automation_system;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
