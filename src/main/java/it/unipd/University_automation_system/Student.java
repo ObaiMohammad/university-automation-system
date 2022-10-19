5f755c2a3db64002c66b4fc232d2f945bad0f24e
ObaiMohammad <obai.mohamed.bis@gmail.com>
2022-10-19 17:01:47 +0200

    Add student class

--- src/main/java/it/unipd/University_automation_system/Student.java
+++ src/main/java/it/unipd/University_automation_system/Student.java
@@ -1,5 +1,46 @@
+
 package it.unipd.University_automation_system;
 
 public class Student {
+
+	private int id;
+	private final String FIRST_NAME;
+	private final String LAST_NAME;
+	private  String email;
+
+	public Student (int id, String FIRST_NAME, String LAST_NAME,String email ) {
+		this.id = id;
+		this.FIRST_NAME = FIRST_NAME;
+		this.LAST_NAME = LAST_NAME;
+		this.email = email;
+
+	}
+
+	public int getId () {
+		return id;	 
+	}
+
+	public String getFirstName () {
+		return FIRST_NAME;	 
+	}    
+
+	public String getLastName () {
+		return LAST_NAME;	 
+	}
+
+	public String getEmail () {
+		return email;	 
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+
+
+
+
 
 }
