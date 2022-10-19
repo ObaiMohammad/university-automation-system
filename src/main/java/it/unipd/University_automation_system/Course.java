fa51990e5475bb62d31d2ade1b19b48759c750ee
ObaiMohammad <obai.mohamed.bis@gmail.com>
2022-10-19 17:21:51 +0200

    Add Course class

---
+++ src/main/java/it/unipd/University_automation_system/Course.java
@@ -0,0 +1,29 @@
+package it.unipd.University_automation_system;
+
+public class Course {
+	private int id;
+	private String name;
+	private int credit;
+
+	public Course (int id, String name, int cridet) {
+		this.id = id;
+		this.name = name;
+		this.credit = cridet;
+	}
+	
+	public int getId () {
+		return this.id;
+	}
+
+	public int getCredit () {
+		return this.credit;
+	}
+	public String getName () {
+		return this.name;
+	}
+
+	
+	public void setCredit (int credit) {
+		this.credit = credit;
+	}
+}
