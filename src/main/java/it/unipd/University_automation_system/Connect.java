a0a27d90f5fea4f3aabf1d8f779b6e06c41f9c71
ObaiMohammad <obai.mohamed.bis@gmail.com>
2022-11-06 21:56:30 +0100

    Conect to data base

---
+++ src/main/java/it/unipd/University_automation_system/Connect.java
@@ -0,0 +1,27 @@
+package it.unipd.University_automation_system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+
+    private static String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static String user = "postgres";
+    private static String password = "0000";
+
+    public static Connection connect(){
+
+        Connection cont = null;
+
+        try {
+            cont = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected Successfully");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cont;
+    }
+
+}
--- src/main/java/it/unipd/University_automation_system/main.java
+++ src/main/java/it/unipd/University_automation_system/main.java
@@ -4,45 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Course foundationCS = new Course(001,"Foundation of computer seince",12 );
-		Course dataStructures1 = new Course(002,"Data structures and Algorithm",12 );
-		Course dataStructures2 = new Course(003,"Data structures and Algorithm",6 );
-		Course math = new Course(004,"mathimatic",6 );
-		
-		Student obai = new Student (001,"Obai","Mohammad","obai@gmail.com");
-		Student odai = new Student (002,"Odai","Mohammad","odai@yahoo.com");
-		Student iyad = new Student (003,"Iyad","Zeb","Iyad@gmail.com");
-		
-		Teacher alex = new Teacher (011,"Alexander", "The first","alex@gmail.com");
-		Teacher john = new Teacher (012,"John", "Snow","john@gmail.com");
-		Teacher jack = new Teacher (013,"Jack", "Black","jack@gmail.com");
-		
-
-		CourseStudentServices.addCourseStudent(foundationCS,obai);
-		CourseStudentServices.addCourseStudent(foundationCS,odai);
-		CourseStudentServices.addCourseStudent(dataStructures1,obai);
-		CourseStudentServices.addCourseStudent(dataStructures2 , odai);
-		CourseStudentServices.addCourseStudent(dataStructures2 , iyad);
-		CourseStudentServices.addCourseStudent(math , obai);
-		CourseStudentServices.addCourseStudent(math , odai);
-		CourseStudentServices.addCourseStudent(math , iyad);
-				
-		CourseTeacherServices.addCourseTeacher(math, jack);
-		CourseTeacherServices.addCourseTeacher(foundationCS, alex);
-		CourseTeacherServices.addCourseTeacher(dataStructures1, john);
-		CourseTeacherServices.addCourseTeacher(dataStructures2, john);
-		CourseTeacherServices.addCourseTeacher(dataStructures2, alex);
-		
-		
-		CourseStudentServices.setGradeOneLineSimplified(001,001,70);
-		CourseStudentServices.setGradeOneLineSimplified(002,001,80);
-		CourseStudentServices.setGradeOneLineSimplified(004,001,50);
-		
-		 System.out.println(CourseStudentServices.getGrade(001,001));
-		 
-		 BookletServices.getBooklet(001);
-		
-		
+		Connect.connect();
 	}
 
 
